3bc665cf8f8675a7ba8b53f30a42968d8eecada0
WUGUOKAI <guokai.wu@hand-china.com>
2017-08-04 20:07:35 +0800

    [ADD]新建了spring-boot项目

---
+++ src/main/java/com/wisely/ch5_2_3/Ch523Application.java
@@ -0,0 +1,12 @@
+package com.wisely.ch5_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch523Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch523Application.class, args);
+	}
+}
